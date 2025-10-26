b415905123e3392fade3afc091def1fd09ef3009
iamrafsanrafi <arafat.yasir1305@gmail.com>
2025-10-26 06:13:59 +0600

    Top stories & market quotes added with trading view

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import TradingViewWidget from "@/components/TradingViewWidget"
-import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants"
+import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from "@/lib/constants"
 
 const HomePage = () => {
   const baseScriptUrl = "https://s3.tradingview.com/external-embedding/embed-widget-";
@@ -21,8 +21,26 @@ const HomePage = () => {
           <TradingViewWidget
             title="Stock Heatmap"
             scriptUrl={baseScriptUrl + "stock-heatmap.js"}
-            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            config={HEATMAP_WIDGET_CONFIG}
             className="custom-chart"
+            height={600}
+          />
+        </div>
+      </section>
+
+      <section className="grid w-full gap-8 home-section">
+        <div className="h-full md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            scriptUrl={baseScriptUrl + "timeline.js"}
+            config={TOP_STORIES_WIDGET_CONFIG}
+            height={600}
+          />
+        </div>
+
+        <div className="h-full md:col-span-1 xl:col-span-2">
+          <TradingViewWidget
+            scriptUrl={baseScriptUrl + "market-quotes.js"}
+            config={MARKET_DATA_WIDGET_CONFIG}
             height={600}
           />
         </div>
